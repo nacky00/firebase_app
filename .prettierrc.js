ea05550a4eac2ccd88b27d97e029cae6e7ed1723
相田夏紀 <natsuki@nacky.local>
2018-12-23 22:17:32 +0900

    chore:[N-3] add eslint, prettier and flow
    SSIA

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  useTabs: false,
+  singleQuote: false,
+  semi: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always'
+};
