6f9c08c62027aaddca3b3c365d47033917bb16e2
相田夏紀 <natsuki@nacky.local>
2018-12-23 22:17:32 +0900

    chore:[N-3] add eslint, prettier and flow
    SSIA

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "env": {
+        "browser": true,
+        "es6": true,
+        "jest": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:flowtype/recommended",
+        "plugin:prettier/recommended",
+        "prettier",
+        "prettier/flowtype",
+        "prettier/react"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "flowtype",
+        "prettier"
+    ],
+    "rules": {
+        "prettier/prettier": ['error', { "singleQuote": true }],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "react/prop-types": [
+            "off"
+        ],
+        "no-var": [
+            "error"
+        ],
+        "no-console": "off",
+        "no-debugger": "off",
+        "no-unused-vars": [
+            "warn"
+        ],
+    }
+};
