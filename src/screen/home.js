55a8e8f93263eb22a863f25e7efecce2f1c8781e
nacky <natsuki@nacky.local>
2018-12-24 10:48:55 +0900

    feat:[N-8] git commit all files
    移動できなかった

---
+++ src/screen/home.js
@@ -0,0 +1,11 @@
+//@flow
+import React, { Component } from "react";
+import Button from "@material-ui/core";
+
+type Props = {};
+
+class Home extends Component<Props> {
+  constructor(props) {
+    super(props);
+  }
+}
