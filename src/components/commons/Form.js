3495b6b770d55a478d195995e1221b460bf14578
nacky <natsuki@nacky.local>
2019-01-14 16:29:43 +0900

    feat:[N-19] modify text field component
    SSIA

--- src/components/commons/Form.js
+++ src/components/commons/Form.js
@@ -26,13 +26,28 @@ class Form extends Component<Props, State> {
       name: "",
     };
   }
+
+  handleChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
   render() {
     const { name } = this.state;
     const { classes } = this.props;
     return (
       <Grid container direction="column">
         <Grid item className={classes.name}>
-          <TextField id="name" label="なまえをいれてね" type="string" value={name} variant="outlined" />
+          <TextField
+            id="name"
+            label="なまえをいれてね"
+            type="string"
+            value={name}
+            variant="outlined"
+            onChange={this.handleChange}
+          />
+        </Grid>
+        <Grid item className={classes.name}>
+          <Typography>いまどんなきもち？</Typography>
         </Grid>
         <Grid container spacing={16} wrap="nowrap" justify="center">
           <Grid item>
